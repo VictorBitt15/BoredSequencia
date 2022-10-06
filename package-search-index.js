a47e45b4454343ec26d523e68af2ffbd2167e39f
VictorBitt15 <victor.bmv@hotmail.com>
2022-10-05 22:17:21 -0300

    Update

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.ufrn.imd"},{"l":"br.ufrn.imd.domain"},{"l":"br.ufrn.imd.service"}];updateSearchResults();
--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"br.ufrn.imd.domain","l":"BoredQuote"},{"p":"br.ufrn.imd.service","l":"ConsumerBored"},{"p":"br.ufrn.imd","l":"Main"}];updateSearchResults();
